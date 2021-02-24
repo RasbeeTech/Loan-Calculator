8982a6980e88665386296b73d5d8d7b66b4fd554
MykalB081 <78550307+MykalB081@users.noreply.github.com>
2021-02-24 18:19:04 -0500

    Update Loan-Calculator.py
    - GUI added

--- Loan-Calculator.py
+++ Loan-Calculator.py
@@ -1,3 +1,5 @@
+import tkinter as tk
+
 loan_interest = 5.99
 loan_principle = 15559.99
 months = 60
@@ -8,44 +10,59 @@ amortized = True
 
 # Formula: Loan Payment(p) = Amount(a) / Discount Factor(d)
 # Calculate monthly payment amount
+def interest_only_calculated(total_loan_amount=loan_principle, interest_rate=loan_interest, number_of_payments=months):
+    a = total_loan_amount
+    n = number_of_payments
+    r = interest_rate
+    print("Interest Only:")
+    monthly_payments = a * (r / n)
+    print("Monthly:", monthly_payments)
+
+
 def amortized_calculated(total_loan_amount, interest_rate, number_of_payments):
     a = total_loan_amount
     n = number_of_payments
     if interest_rate > 1:
         r = interest_rate / 100
         r /= 12
+    print("Amortized\n")
     print("Loan:", a)
     print("Payments:", n)
-    print("Interest:", r)
+    print("Interest:", interest_rate, "%")
 
-    l = r + 1
-    q = l ** n
+    # monthly payments:
+    monthly_payments = a * ((r * (1 + r) ** n) / (((1 + r) ** n) - 1))
+    print("monthly: $", round(monthly_payments, 2))
 
-    m = l ** n
-
-    t = m * r
-
-    print("n:", n)
-    print("r:", r)
-    print("l:", l)
-    print("q:", q)
-    print("m:", m)
-    print("t:", t)
-
-    final = q / t
-    print("final:", final)
-
-    d = ((1 + r) ** n) / (r * (1 + r) ** n)
-    print("Discount:", d)
-    #monthly payments:
-    lp = a * ((r*(1+r)**n)/(((1+r)**n)-1))
-    print("monthly:",round(lp,2))
-
-    print("total",round((lp*n)))
-    p = a / d
-    print("payment:", p)
+    print("total interest: $", round(((monthly_payments * n) - a), 2))
+    print("total: $", round((monthly_payments * n)))
 
 
 if __name__ == '__main__':
     print("Loan Calculator\n")
     amortized_calculated(loan_principle, loan_interest, months)
+
+    window = tk.Tk()
+    window.title("Loan Calculator")
+
+    # labels
+    label_loan_amount = tk.Label(text="Loan Amount($):").grid(row=0, column=0)
+    label_interest = tk.Label(text="Interest Rate(%):").grid(row=1, column=0)
+    label_num_payments = tk.Label(text="Length(months):").grid(row=2, column=0)
+
+    # entries
+    entry_loan_amount = tk.Entry(bd=3)
+    entry_loan_amount.insert(index=tk.END, string=str(loan_principle))
+    entry_interest = tk.Entry(bd=3)
+    entry_interest.insert(index=tk.END, string=str(loan_interest))
+    entry_num_payments = tk.Entry(bd=3)
+    entry_num_payments.insert(index=tk.END, string=str(months))
+
+    entry_loan_amount.grid(row=0, column=1)
+    entry_interest.grid(row=1, column=1)
+    entry_num_payments.grid(row=2, column=1)
+
+    # Calculate button
+    btn_calculate = tk.Button(text="Calculate", command=lambda: amortized_calculated(loan_principle, loan_interest, months))
+    btn_calculate.grid(columnspan=2, row=3, column=0, pady=20)
+    window.mainloop()
