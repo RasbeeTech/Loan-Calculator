10b049e207b2bcc6ceead909f43e4d1f550af3df
MykalB081 <78550307+MykalB081@users.noreply.github.com>
2021-02-24 19:02:40 -0500

    Update Loan-Calculator.py

--- Loan-Calculator.py
+++ Loan-Calculator.py
@@ -1,4 +1,29 @@
 import tkinter as tk
+# pip install matplotlib
+import matplotlib.pyplot as plt
+
+
+def plot_graph():
+    # x axis values
+    x = [1, 2, 3]
+    # corresponding y axis values
+    y = [2, 4, 1]
+
+    # plotting the points
+    plt.plot(x, y)
+
+    # naming the x axis
+    plt.xlabel('Year')
+    # naming the y axis
+    plt.ylabel('Amount($)')
+
+    # giving a title to my graph
+    plt.title('Loan Calculator')
+
+    # function to show the plot
+    #plt.show()
+    return plt
+
 
 loan_interest = 5.99
 loan_principle = 15559.99
@@ -45,10 +70,13 @@ if __name__ == '__main__':
     window = tk.Tk()
     window.title("Loan Calculator")
 
+    plot = plot_graph()
+
+
     # labels
-    label_loan_amount = tk.Label(text="Loan Amount($):").grid(row=0, column=0)
-    label_interest = tk.Label(text="Interest Rate(%):").grid(row=1, column=0)
-    label_num_payments = tk.Label(text="Length(months):").grid(row=2, column=0)
+    label_loan_amount = tk.Label(text="Loan Amount($):", font="Helvetica 13 bold").grid(row=0, column=0)
+    label_interest = tk.Label(text="Interest Rate(%):", font="Helvetica 13 bold").grid(row=1, column=0)
+    label_num_payments = tk.Label(text="Length(months):", font="Helvetica 13 bold").grid(row=2, column=0)
 
     # entries
     entry_loan_amount = tk.Entry(bd=3)
@@ -61,8 +89,13 @@ if __name__ == '__main__':
     entry_loan_amount.grid(row=0, column=1)
     entry_interest.grid(row=1, column=1)
     entry_num_payments.grid(row=2, column=1)
+    option_mnths_yrs = tk.Menu(window)
+    # option_mnths_yrs.add_command("one")
+    # option_mnths_yrs.grid(row=2,column=3)
 
     # Calculate button
-    btn_calculate = tk.Button(text="Calculate", command=lambda: amortized_calculated(loan_principle, loan_interest, months))
+    btn_calculate = tk.Button(text="Calculate",
+                              command=lambda: amortized_calculated(loan_principle, loan_interest, months),
+                              font="Helvetica 15 bold")
     btn_calculate.grid(columnspan=2, row=3, column=0, pady=20)
     window.mainloop()
