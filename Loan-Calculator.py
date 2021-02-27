c1bff96f34e431ea31d7f218b2490657eb210974
MykalB081 <78550307+MykalB081@users.noreply.github.com>
2021-02-27 09:46:13 -0500

    Update Loan-Calculator.py
    - get plot points
    - plot graph

--- Loan-Calculator.py
+++ Loan-Calculator.py
@@ -10,22 +10,24 @@ matplotlib.use('TkAgg')
 
 class Loan:
     def __init__(self, interest, principle, payment_type, term):
-        self.loan_interest = interest/12/100
+        self.loan_interest = interest / 12 / 100
         self.loan_principle = principle
         self.months = term
         if payment_type == "monthly":
             self.months = term
-        #interest_rate = interest / 100
+        # interest_rate = interest / 100
         self.monthly_payments = principle * (
-                    (self.loan_interest * (1 + self.loan_interest) ** term) / (((1 + self.loan_interest) ** term) - 1))
+                (self.loan_interest * (1 + self.loan_interest) ** term) / (((1 + self.loan_interest) ** term) - 1))
         self.loan_total = self.monthly_payments * term
         amortized = True
         self.print_attributes()
+        #self.get_plot_points_yearly()
+
 
     def print_attributes(self):
         print("Loan")
         print("Principle: ", self.loan_principle)
-        print("Interest: ", self.loan_interest)
+        print("Interest: ", self.loan_interest * 12 * 100)
         print("Total: ", self.loan_total)
         print("Monthly: ", self.monthly_payments)
 
@@ -37,62 +39,79 @@ class Loan:
         count_y = 0
         payments = []
         while count_x > count_y:
+
             payments.append(
                 {
                     "#": count_y,
                     "paid": round(m_payment, 2),
-                    "total_paid": round(m_payment*(count_y+1), 2),
-                    "interest": round((self.loan_interest * m_principle)+interest_paid,2),
-                    "toward_principle": round(m_payment-(self.loan_interest * m_principle), 2),
-                    "principle": round(m_principle-m_payment, 2)
+                    "total_paid": round(m_payment * (count_y + 1), 2),
+                    "interest": round((self.loan_interest * m_principle) + interest_paid, 2),
+                    "toward_principle": round(m_payment - (self.loan_interest * m_principle), 2),
+                    "principle": round(m_principle - (m_payment - (self.loan_interest * m_principle)), 2)
                 }
             )
             interest_paid += (self.loan_interest * m_principle)
-            m_principle -= (m_payment - (m_principle * self.loan_interest))
             if m_payment > m_principle:
-                m_payment = m_principle+(m_principle*self.loan_interest)
+                m_payment = m_principle + (m_principle * self.loan_interest)
+            m_principle -= (m_payment - (m_principle * self.loan_interest))
+
             count_y += 1
-        for i in payments:
-            print(i)
+        return payments
+        #for i in payments:
+            #print(i)
 
+    def get_plot_points_yearly(self):
+        # get x points
+        points = self.months / 12
+        x = list(range(1, (int(points) + 1)))
+        #while points > 0:
+        #    x.insert(0, points)
+        #    points -= 1
+        print(x)
 
+        # get y points
+        data = self.get_amount_paid()
+        d = data[11::12]
+        y_total_paid = []
+        y_interest_paid = []
+        y_principle = []
+        for points in d:
+            y_total_paid.append(points.get("total_paid"))
+            y_interest_paid.append(points.get("interest"))
+            y_principle.append(points.get("principle"))
 
-def plot_graph():
-    x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
-    v = np.array([16, 16.31925, 17.6394, 16.003, 17.2861, 17.3131, 19.1259, 18.9694, 22.0003, 22.81226])
-    p = np.array([1, 16.23697, 17.31653, 17.22094, 17.68631, 17.73641, 18.6368,
-                  19.32125, 19.31756, 21.20247, 22.41444, 22.11718, 22.12453])
+        print("y_total_paid: ", y_total_paid)
+        # print("y_interest_paid: ", y_interest_paid)
+        # print("y_principle: ", y_principle)
+        plot_points = [x, y_total_paid]
+        print(plot_points)
+        return plot_points
 
-    fig = Figure(figsize=(5, 5))
-    a = fig.add_subplot(111)
-    # a.scatter(v, x, color='red')
-    a.plot(range(2 + max(x)), p, color='red')
-    # a.invert_yaxis()
+    def get_plot_graph(self):
+        x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        v = np.array([16, 16.31925, 17.6394, 16.003, 17.2861, 17.3131, 19.1259, 18.9694, 22.0003, 22.81226])
+        p = np.array([16.23697, 17.31653, 17.22094, 17.68631, 17.73641, 18.6368,
+                      19.32125, 19.31756, 21.20247, 22.41444, 22.11718, 22.12453])
+        print("p: ", p)
+        plot_points = self.get_plot_points_yearly()
 
-    a.set_title("Loan Calculator", fontsize=16)
-    a.set_ylabel("Amount ($)", fontsize=14)
-    a.set_xlabel("Year", fontsize=14)
+        x = np.array(plot_points[0])
+        y = np.array(plot_points[1])
+        print("x: ", x)
+        print("y: ", y)
 
-    return fig
+        fig = Figure(figsize=(5, 5))
+        a = fig.add_subplot(111)
+        a.scatter(x, y, color='red')
+        a.plot (x,y, color="blue")
+        # a.plot(range(2 + max(x)), y, color='red')
+        # a.invert_yaxis()
 
+        a.set_title("Loan Calculator", fontsize=16)
+        a.set_ylabel("Amount ($)", fontsize=14)
+        a.set_xlabel("Year", fontsize=14)
 
-def get_plot_points(obj):
-    # Get x values
-    points = obj.months / 12
-    x = []
-    while points > 0:
-        x.insert(0, points)
-        points -= 1
-    print(x)
-
-    # Get y values
-    points = obj.loan_total
-    point = obj.loan_total / len(x)
-    y = []
-    count = len(x)
-    # while points > 0:
-    #   y.append(points-(point*))
-    print(y)
+        return fig
 
 
 if __name__ == '__main__':
@@ -101,8 +120,6 @@ if __name__ == '__main__':
     loan = Loan(interest=5.99, principle=15559.99, payment_type="monthly,", term=60)
     window = tk.Tk()
     window.title("Loan Calculator")
-
-    plot = plot_graph()
 
     # labels
     label_loan_amount = tk.Label(text="Loan Amount($):", font="Helvetica 13 bold").grid(row=0, column=0)
@@ -131,11 +148,11 @@ if __name__ == '__main__':
     btn_calculate.grid(columnspan=2, row=3, column=0, pady=20)
 
     # place plot graph in tkinter window
-    plot_graph = plot_graph()
+    plot_graph = loan.get_plot_graph()
     canvas = FigureCanvasTkAgg(plot_graph, master=window)
     canvas.get_tk_widget().grid(columnspan=30, rowspan=30, row=0, column=3)
     canvas.draw()
 
-    get_plot_points(loan)
-    loan.get_amount_paid()
+    # get_plot_points(loan)
+    # loan.get_amount_paid()
     window.mainloop()
